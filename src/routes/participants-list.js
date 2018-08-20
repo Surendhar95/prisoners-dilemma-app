7ba57188d09996790917e03169645dcb52cbef4b
Charles Hill <chill@degreesofzero.com>
2018-08-20 18:40:02 +0200

    Fixed password form

--- src/routes/participants-list.js
+++ src/routes/participants-list.js
@@ -10,7 +10,7 @@ class ParticipantsList extends React.Component {
       started: false
     }
     this.getParticipants().then(data => {
-      this.setState({ participants: data.participants })
+      this.setState({ participants: data })
     })
   }
 
@@ -28,7 +28,8 @@ class ParticipantsList extends React.Component {
     return response.json()
   }
 
-  checkPassword = async () => {
+  checkPassword = async e => {
+    e.preventDefault()
     const { password } = this.state
     const response = await fetch('http://localhost:3010/check-password', {
       method: 'POST',
@@ -77,8 +78,8 @@ class ParticipantsList extends React.Component {
   render() {
     const { participants, password, error, done, started } = this.state
     return (
-      <div style={{ display: 'block', textAlign: 'center' }}>
-        <form onSubmit={this.submit}>
+      <div>
+        <form onSubmit={this.checkPassword}>
           <div className="form">
             <div className="form-error">{error || (done ? 'OK' : '')}</div>
             <div className="form-row">
@@ -113,15 +114,19 @@ class ParticipantsList extends React.Component {
         </div>
         <div>
           <table>
-            <th style={{ textAlign: 'center' }}>
-              <td>Participants</td>
-            </th>
-            {participants &&
-              participants.map(participant => (
-                <tr>
-                  <td>{participant.alias}</td>
-                </tr>
-              ))}
+            <thead>
+              <tr>
+                <th>Participants</th>
+              </tr>
+            </thead>
+            <tbody>
+              {participants &&
+                participants.map(participant => (
+                  <tr key={participant.alias}>
+                    <td>{participant.alias}</td>
+                  </tr>
+                ))}
+            </tbody>
           </table>
         </div>
       </div>
